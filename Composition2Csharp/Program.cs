2972931c194efc96104f9d0faeedfc830e11fa89
Lucas Ortis <lucasortis01@gmail.com>
2021-09-02 18:00:22 -0300

    deletando para refatorar e movendo os arquivos .ggitignore e .git para o local correto

---
+++ Composition2Csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Composition2Csharp.Entities;
+
+namespace Composition2Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
